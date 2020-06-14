19c0db67753188bad73999de02edb16054cf01f2
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2020-06-14 11:51:31 -0300

    Implementa ex.02 do módulo 04 - Js Assíncrono

--- 4-js-assincrono/exercicio-2/ex2.js
+++ 4-js-assincrono/exercicio-2/ex2.js
@@ -1,7 +1,10 @@
 (function() {
-    let atv2 = document.getElementById('atv2')
+    let atv2 = document.getElementById('atv2');
+    let div1 = document.getElementById('div1');
     let rowIpt = document.createElement('input');
     let rowBtn = document.createElement('button');
+    let ul = document.createElement('ul');
+    div1.appendChild(ul);
     rowIpt.setAttribute('placeholder', 'Buscar com nome de usuário pela api do Github');
 
     atv2.appendChild(rowIpt);
@@ -11,4 +14,28 @@
     let txtBtn = document.createTextNode('Buscar');
     rowBtn.appendChild(txtBtn);
 
+    rowBtn.addEventListener('click', function() {
+        let user = rowIpt.value;
+
+        ul.textContent = ""
+
+        axios.get(`https://api.github.com/users/${user}/repos`)
+            .then(function(response){    
+                let acess = Object.values(JSON.parse(response.request.responseText));
+                acess.forEach(element => {
+                    let li = document.createElement('li');
+                    let text = document.createTextNode(element.name)
+                    li.appendChild(text);
+                    ul.appendChild(li);
+                });
+            })
+            .catch(function(){
+                let li = document.createElement('li');                
+                let txtEmpty = document.createTextNode("Usuário inexistente!")
+                li.appendChild(txtEmpty);
+                ul.appendChild(li);
+            })
+
+    });
+    
 })();
